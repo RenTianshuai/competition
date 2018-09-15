e652e3710fb82d2888193155b2f2c640075588de
rts <rents@yusys.com.cn>
2018-09-15 12:00:52 +0800

    commit

--- institutionalIndexRanking/src/main/java/cn/com/yusys/geek/subject1/utils/SQLUtil.java
+++ institutionalIndexRanking/src/main/java/cn/com/yusys/geek/subject1/utils/SQLUtil.java
@@ -1,31 +1,19 @@
 package cn.com.yusys.geek.subject1.utils;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 /**
  *
  */
 public class SQLUtil {
 
-    // 测试方法
-    public static void main(String[] args) throws Exception {
-
-    }
+    private static String url = "jdbc:mysql://127.0.0.1:3306/message";
+    private static String user = "root";
+    private static String password = "admin";
 
 
     /**
@@ -39,7 +27,7 @@ public class SQLUtil {
         PreparedStatement pst = null;
         ResultSet rs = null;
         Connection conn = getConnection();
-        List<Map<String, Object>> resultList = new ArrayList<>();
+        List<Map<String, Object>> resultList = new ArrayList<Map<String,Object>>();
         // Map<String, Object> result = new HashMap<>();
 
         try {
@@ -57,7 +45,7 @@ public class SQLUtil {
 
             while (rs.next()) {
                 Map<String, Object> result = new HashMap<String, Object>();
-                for (int i = 1; i < count; i++) {
+                for (int i = 1; i <= count; i++) {
                     //获取指定列的表目录名称
                     String label = rsmd.getColumnLabel(i);
                     //以 Java 编程语言中 Object 的形式获取此 ResultSet 对象的当前行中指定列的值
@@ -102,20 +90,8 @@ public class SQLUtil {
     }
 
     public static Connection getConnection() {
-        String url = "jdbc:oracle:thin:@localhost:1521:orcl";
-        url = "jdbc:mysql://localhost:3306/glloans";
-        //用户名
-        String user = "root";
-        //密码
-        String password = "root";
-        // user="cmis_user";
-        // password="123qweasd";
-        // url = "jdbc:mysql://rm-bp1ta22u8bf41t145.mysql.rds.aliyuncs.com:3306/glloans";
         Connection connection = null;
         try {
-            //1.加载驱动
-            // Class.forName("oracle.jdbc.driver.OracleDriver");
-            //2.得到连接
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(url, user, password);
             return connection;
@@ -136,6 +112,4 @@ public class SQLUtil {
             e.printStackTrace();
         }
     }
-
-
 }
