792b1eadc069af804bfdd5a70a0f1e31ddf916c8
rts <rents@yusys.com.cn>
2018-09-15 10:25:37 +0800

    init

---
+++ institutionalIndexRanking/src/main/java/cn/com/yusys/geek/subject1/Ranking.java
@@ -0,0 +1,49 @@
+package cn.com.yusys.geek.subject1;
+
+
+import java.sql.*;
+
+public class Ranking {
+    private static String url = "jdbc:mysql://localhost:3306/message";
+    private static String username = "root";
+    private static String password = "admin";
+    public static void main(String[] args){
+        Connection conn = null;
+        String sql;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");// 动态加载mysql驱动
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            StringBuffer sql = new StringBuffer();
+
+            int result = stmt.executeUpdate(sql);// executeUpdate语句会返回一个受影响的行数，如果返回-1就没有成功
+            if (result != -1) {
+                System.out.println("创建数据表成功");
+                sql = "insert into student(NO,name) values('2012001','陶伟基')";
+                result = stmt.executeUpdate(sql);
+                sql = "insert into student(NO,name) values('2012002','周小俊')";
+                result = stmt.executeUpdate(sql);
+                sql = "select * from student";
+                ResultSet rs = stmt.executeQuery(sql);// executeQuery会返回结果的集合，否则返回空值
+                System.out.println("学号\t姓名");
+                while (rs.next()) {
+                    System.out
+                            .println(rs.getString(1) + "\t" + rs.getString(2));// 入如果返回的是int类型可以用getInt()
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
+}
