3454732c120d72d3e9a00bcfa0b0aefb3652f170
rts <rents@yusys.com.cn>
2018-09-17 14:40:31 +0800

    ss

--- datacount/src/main/java/com/yusys/geek/DataCount.java
+++ datacount/src/main/java/com/yusys/geek/DataCount.java
@@ -2,6 +2,8 @@ package com.yusys.geek;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 入口类
@@ -13,6 +15,8 @@ public class DataCount {
     private synchronized void countAdd(String key, int addValue){
         count.put(key,count.get(key)+addValue);
     }
+    private AtomicInteger outCount = null;
+    private AtomicInteger succCount = null;
     /**
      * 入口方法
      * @param rpcNum 接口数量
@@ -25,6 +29,8 @@ public class DataCount {
         count = new HashMap<String, Integer>();
         count.put("outCount",0);
         count.put("succCount",0);
+        outCount = new AtomicInteger(0);
+        succCount = new AtomicInteger(0);
         for (int i=0;i<rpcNum;i++){
             new Thread(new Runnable() {
                 public void run() {
@@ -34,9 +40,11 @@ public class DataCount {
                         long end = System.currentTimeMillis();
                         long time = (end-start)/1000;
                         if (time > timeOut){
-                            countAdd("outCount",1);
+//                            countAdd("outCount",1);
+                            outCount.incrementAndGet();
                         }else {
-                            countAdd("succCount",1);
+//                            countAdd("succCount",1);
+                            succCount.incrementAndGet();
                         }
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -44,20 +52,25 @@ public class DataCount {
                 }
             }).start();
         }
-        while (Thread.activeCount() > 1){
+//        while (rpcNum > (count.get("outCount"))+(count.get("succCount"))){
+        while (rpcNum > (outCount.get() + succCount.get())){
         }
         System.out.println("统计用时："+(System.currentTimeMillis()-s)/1000 + "秒");
-        String result = count.get("outCount")+","+count.get("succCount");
+//        String result = count.get("outCount")+","+count.get("succCount");
+        String result = outCount.get()+","+succCount.get();
         System.out.println("最后结果："+result);
         return result;
     }
 
     public static void main(String[] args){
         DataCount dc = new DataCount();
-//        for(int i=0;i<10;i++){
-            dc.process(92,2,5);
+        Random r = new Random();
+        for(int i=0;i<10;i++){
+            int c = r.nextInt(100);
+            System.out.println("第"+(i+1)+"组："+c);
+            dc.process(c,2,5);
             System.out.println("调用次数："+RPC.getCount());//其中p1,p2,p3随机生成
-//        }
+        }
 
     }
 }
